7e09ac11f550b600918218da85b36e01ecfaa465
fatima <fatimahassanhassab8@gmail.com>
2024-03-10 06:35:38 -0700

    get student from render

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-alert('hello world');
+function RenderStudents(){
+ddl_students=document.getElementById('ddl_students');
+fetch('https://hostingusingrender-session-seven.onrender.com/students')
+      .then(response => response.json())
+      .then(data => data.forEach(students => {
+let option=document.createElement('option');
+option.innerHTML=students.name;
+ddl_students.appendChild(option);
+      })
+     )};
+     RenderStudents();
+     
